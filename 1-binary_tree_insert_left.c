e64c80690a373624b62f6fa9d5e116e568b4c556
lawsonredeye <omoregbeeolawson@gmail.com>
2024-02-28 22:03:02 +0100

    created a new node instead of malloc

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,15 +13,15 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 		return (NULL);
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = value;
 	if (parent->left != NULL)
 	{
-		new_node->parent = parent;
 		new_node->left = parent->left;
 		new_node->left->parent = new_node;
+		new_node->parent = parent;
 		parent->left = new_node;
 	}
 	else
