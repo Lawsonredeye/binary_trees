8b2b4e57478bafd462c8a23d40e5cb91068638b1
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-01 00:03:40 +0100

    modified

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,16 +8,7 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	int ldepth, rdepth;
-
-        if (tree == NULL)
-                return (0);
-
-
-        ldepth = binary_tree_depth(tree->left);
-        rdepth = binary_tree_depth(tree->right);
-        if (ldepth > rdepth)
-                return (ldepth + 1);
-        else
-                return (rdepth + 1);
+	if (tree == NULL)
+		return (-1);
+	return ( 1 + binary_tree_depth(tree->parent));
 }
