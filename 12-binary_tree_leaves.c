c48e8889716814b847b7ae28184a501cc1c6677e
lawsonredeye <omoregbeeolawson@gmail.com>
2024-02-29 15:20:52 +0100

    fixed betty

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,8 +1,14 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_leaves - function that counts the leaves in a binary tree
+ * @tree: root node of the tree to count the number of leaves
+ * Return: sum of the left and right leafs
+ */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t leftleaf, rightleaf, total;
+
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
