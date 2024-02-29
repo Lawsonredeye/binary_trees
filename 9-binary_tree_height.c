24891d2497bef655ffd11b359c807ba7785949c9
lawsonredeye <omoregbeeolawson@gmail.com>
2024-02-29 21:32:06 +0100

    used if statements

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,8 +13,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (-1);
 
-	Lside = binary_tree_height(tree->left);
-	Rside = binary_tree_height(tree->right);
+	if (tree->left)
+		Lside = binary_tree_height(tree->left);
+	if (tree->right)
+		Rside = binary_tree_height(tree->right);
 	if (Rside >= Lside)
 		return (Lside + 1);
 	else
