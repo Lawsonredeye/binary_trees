6d99781d06666dd568cf79615a0080fb5830bec2
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-01 00:04:27 +0100

    fixed prohibited space

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,5 +10,5 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (-1);
-	return ( 1 + binary_tree_depth(tree->parent));
+	return (1 + binary_tree_depth(tree->parent));
 }
