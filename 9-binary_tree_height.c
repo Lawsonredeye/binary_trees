b79ea933e615a75393e6838ef18ed4db33f54268
lawsonredeye <omoregbeeolawson@gmail.com>
2024-02-29 21:33:09 +0100

    fixed

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,14 +11,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t Lside, Rside;
 
 	if (tree == NULL)
-		return (-1);
+		return (0);
 
 	if (tree->left)
 		Lside = binary_tree_height(tree->left);
 	if (tree->right)
 		Rside = binary_tree_height(tree->right);
 	if (Rside >= Lside)
-		return (Lside + 1);
-	else
 		return (Rside + 1);
+	else
+		return (Lside + 1);
 }
